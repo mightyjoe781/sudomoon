1add5f2a6ffc8f7df48a5a2d1749aa970ad93afb
smk <sudhanshumohan781@gmail.com>
2024-02-20 16:03:11 +0530

    Basic Structure Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,33 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypePresetMinify from 'rehype-preset-minify';
+import react from '@astrojs/react'
+
+
+const rehypeExternalLinksConfig = [
+	rehypeExternalLinks,
+	{ target:  '_blank', rel: ['noopener', 'noreferrer']}
+]
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sudhanshu.dev',
-	integrations: [mdx(), sitemap()],
+	trailingSlash: 'never',
+	output: 'server',
+	integrations: [mdx({
+		rehypePlugins: [rehypeExternalLinksConfig, rehypePresetMinify]
+	}), 
+	react(),
+	sitemap({
+		filter: (page) => !page.includes('--delist')
+	})],
+	markdown: {
+		smartypants: true,
+		rehypePlugins: [rehypeExternalLinksConfig],
+		shikiConfig: {
+			theme: 'one-dark-pro'
+		}
+	}
 });
